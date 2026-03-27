7d5b726aa821fcde08bb7753542c2d1467d94cd2
OZENITRAM <oliveremartinez09@gmail.com>
2026-03-27 16:45:35 -0300

    Add files via upload

---
+++ 6_Martinez_V/TP3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp3_oliver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+      
+            Console.WriteLine("hola papu");
+            Console.Write("¿Cual es tu año en que naciste?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+          
+
+            int edad = 2026 - año;
+          Console.WriteLine("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años ");
+
+        }
+    }
+}
