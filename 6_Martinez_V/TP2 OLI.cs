85eee6835d4153af47aa479fd4b49842201e362a
OZENITRAM <oliveremartinez09@gmail.com>
2026-03-27 16:27:45 -0300

    Add files via upload

---
+++ 6_Martinez_V/TP2 OLI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_1_Oliver_Martinez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre;
+            string juego;
+            string consola;
+
+            Console.WriteLine("hola papu");
+            Console.Write("¿Cual es tu nombre?: " );
+
+            nombre = Console.ReadLine();
+
+    
+
+            Console.Write("¿Cual es tu juego favorita?: " );
+            juego = Console.ReadLine();
+            Console.Write("¿en que consola jugas?: ");
+            consola = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + " Tu juego favorito es " + juego + " y se juega en " + consola);
+
+
+
+
+
+
+
+
+        }
+    }
+}
