e48b23a4184f3a71790067d4e2f48695bae995ae
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-08 15:25:21 -0300

    Add files via upload

---
+++ 6_Martinez_V/TP5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("¿Cuantos minutos estuvo en el estacionamiento? ");
+            int minutos = Convert.ToInt32(Console.ReadLine());
+
+            int hora = minutos / 60;
+
+
+            int min = minutos % 60;
+
+            Console.WriteLine(hora + " hora/s " + " con " + min + " minutos ");
+
+            int preciohora = hora * 6000;
+            int preciomin = min * 150;
+            int preciofinal = preciohora + preciomin;
+
+
+
+            int iva = preciofinal * 21 / 100;
+
+            Console.WriteLine( iva + " del iva " );
+
+            int montofinal = iva + preciofinal;
+
+            Console.WriteLine( montofinal + " es el precio a pagar" );
+
+        }
+    }
+}
