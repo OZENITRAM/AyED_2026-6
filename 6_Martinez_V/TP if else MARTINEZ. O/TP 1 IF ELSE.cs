8198274aecd6a85536cdbb925c413e1b67aa5815
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-08 16:58:16 -0300

    Add files via upload

---
+++ 6_Martinez_V/TP if else MARTINEZ. O/TP 1 IF ELSE.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuanto  pesa la maleducada?: ");
+
+            int peso = Convert.ToInt32(Console.ReadLine());
+
+
+            if (peso <= 5000)
+
+            {
+
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+
+            }
+             else
+            {
+                int diferenciaDEpeso = peso - 5000;
+                Console.WriteLine(" Alerta: Exceso de peso. Debes descargar " + diferenciaDEpeso + " kg para despegar ");
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ 6_Martinez_V/TP if else MARTINEZ. O/tp3 if else.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("¿Cuantos años luz hay que viajar:  ");
+
+            int años_luz = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("¿Cuantos litros de gasolina hay en el tanque:  ");
+
+
+            int litros = Convert.ToInt32(Console.ReadLine());
+            int xd = litros / años_luz;
+            int alan = 12 - litros;
+            if (xd >= 12)
+
+            {
+
+                Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+
+            }
+
+
+            else
+            {
+
+
+
+                Console.WriteLine("Peligro: Combustible insuficiente, Faltan " + alan + " litros");
+
+            }
+
+
+        }
+    }
+}
