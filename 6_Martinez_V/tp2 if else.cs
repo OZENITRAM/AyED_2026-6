be6cc6d270194d7bca7db6192e5dc9bcb882519a
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-08 16:33:24 -0300

    Add files via upload

---
+++ 6_Martinez_V/tp2 if else.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("ingre la cantidad de nivel de midiclorianos: ");
+
+            int medicloriano = Convert.ToInt32(Console.ReadLine());
+
+
+            if (medicloriano >= 15000)
+
+            {
+
+                Console.WriteLine("Potencial Maestro Jedi detectado. Avisar al Consejo");
+
+            }
+
+
+      else if( 5000 < medicloriano)
+
+            {
+         
+                  
+
+                Console.WriteLine(" Aceptable para entrenamiento Padawan");
+
+
+            }
+
+       else
+            {
+            
+                Console.WriteLine("No posee sensibilidad a la Fuerza");
+            }
+
+           
+        }
+    }
+}
