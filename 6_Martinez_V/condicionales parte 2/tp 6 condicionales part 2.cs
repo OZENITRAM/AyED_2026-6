0a41a042298699f2711faf9021fef8eed22ece3b
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 14:15:15 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales parte 2/tp 6 condicionales part 2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Ingresa tu promedio: ");
+            float promedio = Convert.ToSingle(Console.ReadLine());
+
+
+            Console.Write("Ingresa la distancia de tu casa a la facultad en kilometros: ");
+            float distancia = Convert.ToSingle(Console.ReadLine());
+
+
+            if (promedio > 8 || distancia > 50)
+            {
+                Console.WriteLine("Beca concedida.");
+            }
+            else
+            {
+                Console.WriteLine("Beca no concedida.");
+            }
+        }
+    }
+}
