1eb143aa0eab899fbf9aca8b42fffe8bafa6df1c
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 16:06:11 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales parte 2/tp 9 condicionales part2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+               Console.Write("¿cuantas horas dejaste el auto?: ");
+                int horas = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("¿con cuanta plata abonas?: ");
+                int plata = Convert.ToInt32(Console.ReadLine());
+
+
+                int precioH = 4000;
+                if (horas > 1)
+                {
+                    precioH += (horas - 1) * 1600;
+                }
+
+
+
+            if (plata >= precioH)
+            {
+                int vuelto = plata - precioH;
+                    Console.WriteLine("pago aceptado, el vuelto es: " + vuelto);
+                }
+                else
+                {
+                    int plataF = precioH - plata;
+                    Console.WriteLine("plata insuficiente, falta: " + plataF);
+                }
+            }
+    }
+}
