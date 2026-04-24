9d326565371683941eaddea13245081887320052
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 14:09:19 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales parte 2/tp 5 condicionales part 2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Ingresa una cantidad de segundos: ");
+
+            int segundos = Convert.ToInt32(Console.ReadLine());
+
+            if (segundos > 60)
+            {
+                int resultado = segundos / 60;
+
+
+                Console.WriteLine("tus segundos a minutos son: " + resultado + " min. ");
+            }
+            else
+            {
+                Console.WriteLine("tus segundos son: " + segundos);
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
