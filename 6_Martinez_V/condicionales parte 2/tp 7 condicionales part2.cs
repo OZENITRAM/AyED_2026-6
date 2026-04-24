c373c22f7a5b949901837d66ddb5654ec0fd716a
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 14:27:13 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales parte 2/tp 7 condicionales part2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.Write("Ingresa tu saldo actual: ");
+            double saldo = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("¿Cuanto dinero deseas retirar? ");
+            double retiro = Convert.ToDouble(Console.ReadLine());
+
+
+            if (retiro > saldo)
+            {
+                Console.WriteLine("fondos insuficientes");
+            }
+
+            else if (retiro % 1000 == 0)
+            {
+                Console.WriteLine("operacion realizada con exito. retiraste: " + retiro);
+            }
+            else
+            {
+                Console.WriteLine("solo aceptamos billetes de mil");
+
+
+
+
+            }
+        }
+    }
+}
