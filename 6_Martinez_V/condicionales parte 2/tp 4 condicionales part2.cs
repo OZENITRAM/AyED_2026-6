3791cb665a99083dc0d50deed11e524acc9adad2
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 16:19:35 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales parte 2/tp 4 condicionales part2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresa tu sueldo actual: ");
+            double sueldo = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("Ingresa tus años de antigüedad: ");
+            int antigüedad = Convert.ToInt32(Console.ReadLine());
+
+
+            double aumento;
+            if (antigüedad > 10)
+            {
+                aumento = sueldo * 0.20;
+            }
+            else
+            {
+                aumento = sueldo * 0.05;
+            }
+
+
+            double sueldoNuevo = sueldo + aumento;
+
+
+            Console.WriteLine("Tu sueldo nuevo es: " + sueldoNuevo);
+        }
+
+
+
+
+    }
+    }
