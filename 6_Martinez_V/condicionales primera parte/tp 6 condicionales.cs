a19935b8aff20a543371b8c5c24c10751c5027af
OZENITRAM <oliveremartinez09@gmail.com>
2026-04-24 15:36:24 -0300

    Add files via upload

---
+++ 6_Martinez_V/condicionales primera parte/tp 6 condicionales.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Ingresa el nombre de usuario: ");
+            string usuario = Console.ReadLine();
+
+            Console.Write("Ingresa la contraseña: ");
+            string contraseña = Console.ReadLine();
+
+            if (usuario == "admin" )
+            {
+                if(contraseña == "1234")
+
+                Console.WriteLine("Acceso concedido.");
+                else
+                    Console.WriteLine("Error de credenciales.");
+            }
+            else
+            {
+                Console.WriteLine("Error de credenciales.");
+            }
+        }
+    }
+}
