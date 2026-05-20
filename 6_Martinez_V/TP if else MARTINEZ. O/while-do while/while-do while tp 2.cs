f973d0430af002f9395aee4cb59bb059e30f998f
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-20 16:57:11 -0300

    Add files via upload

---
+++ 6_Martinez_V/TP if else MARTINEZ. O/while-do while/while-do while tp 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+
+            do
+            {
+                Console.Write("Ingrese su calificacion: ");
+                nota = int.Parse(Console.ReadLine());
+                if (nota < 1 || nota > 10)
+                {
+
+                    Console.WriteLine("Error, nota no valida, vuelva a ingresarla: ");
+
+
+
+
+                }
+
+            }
+
+            while (nota < 1 || nota > 10);
+
+            Console.WriteLine("Tu nota es: " + nota);
+        }
+    }
+}
+
+
+      
+
+          
---
+++ 6_Martinez_V/TP if else MARTINEZ. O/while-do while/while-do while tp 3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero; 
+            int total = 0;
+            Console.Write("ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            while (numero > 0)
+
+            {
+                total = total + numero;
+                Console.Write("ingrese otro numero: ");
+                
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("La suma total es de: " + total);
+        }
+    }
+
+}
