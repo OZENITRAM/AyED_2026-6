0cd58a32ac45b248c93f6f4f9bcec6dae15059c4
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-20 16:57:47 -0300

    Add files via upload

---
+++ 6_Martinez_V/while-do while/while-do while tp 4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num;
+            int contador = 1;
+
+            Console.Write("Ingresá un número: ");
+            num = int.Parse(Console.ReadLine());
+
+            while (contador <= 12)
+            {
+                Console.WriteLine(num + " x " + contador + " = " + (num * contador));
+                contador = contador + 1;
+            }
+        }
+    }
+}
---
+++ 6_Martinez_V/while-do while/whle-do while tp 1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Introduce un numero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("comineza la cuenta regresiva:");
+            while (numero >= 0)
+
+              {
+                
+
+                   Console.WriteLine(numero);
+                numero = numero - 1;
+              }
+
+        }
+    }
+}
