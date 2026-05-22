34ceb46432e2ee6a0bd2a07e4e4cc9f401ee23dd
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-22 15:29:10 -0300

    Add files via upload

---
+++ 6_Martinez_V/while-do while/while- do while tp 5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string claveconrrecta = "xd";
+            string clave = " ";
+
+            while (clave != claveconrrecta)
+            {
+                Console.Write("ingrese una clave: ");
+
+                clave = Console.ReadLine();
+            } 
+
+            Console.WriteLine(" acceso permitido ");
+        }
+    }
+}
