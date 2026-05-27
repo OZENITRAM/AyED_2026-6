beb018d08efdd66832c07d8c1a5d59eb4366e669
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-27 16:11:49 -0300

    Add files via upload

---
+++ 6_Martinez_V/while-do while/while- do while tp 6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+
+            int contador = 2;
+
+            Console.Write("ingresa un numero: ");
+
+
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                Console.WriteLine(contador);
+
+                contador = contador + 2;
+
+
+
+            }
+        }
+    }
+
+
+
+
+    }
+
