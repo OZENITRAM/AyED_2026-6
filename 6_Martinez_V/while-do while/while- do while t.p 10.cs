c97f755e665eea8234c46b52f89541ff9957726d
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-27 15:26:49 -0300

    Add files via upload

---
+++ 6_Martinez_V/while-do while/while- do while t.p 10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int A;
+            int B;
+            int resultado = 0;
+            int contador = 1;
+
+            Console.Write("ingresa el primer numero: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo numero: ");
+            B = int.Parse(Console.ReadLine());
+
+            while (contador <= B)
+            {
+                resultado = resultado + A;
+                contador++; ;
+            }
+
+            Console.WriteLine("resultado: " + resultado);
+        }
+    }
+
+}
+   
