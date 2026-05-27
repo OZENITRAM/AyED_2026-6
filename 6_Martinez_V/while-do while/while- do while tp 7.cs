0399d2edef5dff0c63e132141e52b91ee064d623
OZENITRAM <oliveremartinez09@gmail.com>
2026-05-27 14:45:44 -0300

    Add files via upload

---
+++ 6_Martinez_V/while-do while/while- do while tp 7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nota;
+            int contador = 1;
+
+            int suma = 0;
+            int promedio;
+
+
+            while (contador <= 5)
+            {
+                Console.Write("ingrese una nota: ");
+                nota = int.Parse(Console.ReadLine());
+                suma = suma + nota;
+                contador++;
+            }
+
+            promedio = suma / 5;
+            Console.WriteLine("El promedio es de: " + promedio);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 6_Martinez_V/while-do while/while- do while tp 8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int temperatura;
+
+            Console.Write("Ingresá la temperatura inicial: ");
+            temperatura = int.Parse(Console.ReadLine());
+
+            while (temperatura > 24)
+            {
+                temperatura = temperatura - 2;
+
+                Console.WriteLine("Enfriando... Temperatura actual: " + temperatura + "°C");
+            }
+
+            Console.WriteLine("Temperatura ideal alcanzada");
+        }
+    }
+    }
