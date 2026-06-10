dfe306aefe3ad6268621ad2c018da24a0daa9b54
OZENITRAM <oliveremartinez09@gmail.com>
2026-06-10 15:27:28 -0300

    Add files via upload

---
+++ 6_Martinez_V/while Guia N2/while- do while TP5.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string dato = "";
+            int numero;
+
+
+            int positivos = 0;
+
+            int negativos = 0;
+
+
+            int ceros = 0;
+
+            while (dato != "X" && dato != "N")
+            {
+                Console.Write("ingresa un numero (X o N para salir): ");
+                dato = Console.ReadLine();
+
+                if (dato != "X" && dato != "N")
+                {
+                    numero = int.Parse(dato);
+
+                    if (numero > 0)
+                    {
+                        positivos = positivos + 1;
+
+
+
+                    }
+                    else if (numero < 0)
+                    {
+                        negativos = negativos + 1;
+                    }
+                    else
+                    {
+                        ceros = ceros + 1;
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine("Positivos: " + positivos);
+
+            Console.WriteLine("Negativos: " + negativos);
+            Console.WriteLine("Ceros: " + ceros);
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
