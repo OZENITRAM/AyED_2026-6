2b0326f78ad0db3529c7dfeac0097716cd827958
OZENITRAM <oliveremartinez09@gmail.com>
2026-06-15 20:33:59 -0300

    Add files via upload

---
+++ while guia N3/while guia n3 tp8.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+
+            int suma = 0;
+            int cantidad = 0;
+            int mayores = 0;
+
+            int menores = 0;
+
+
+            double promedio;
+
+            while (edad >= 0)
+            {
+                Console.Write("ingresa una edad: (-1 para terminar): ");
+                edad = int.Parse(Console.ReadLine());
+
+                if (edad >= 0)
+                {
+                    suma = suma + edad;
+                    cantidad = cantidad + 1;
+
+                    if (edad >= 18)
+                    {
+                        mayores = mayores + 1;
+                    }
+                    else
+                    {
+                        menores = menores + 1;
+
+                    }
+
+                }
+
+            }
+
+            promedio = (double)suma / cantidad;
+
+            Console.WriteLine("promedio de edad: " + promedio);
+
+            Console.WriteLine("mayores de edad: " + mayores);
+            Console.WriteLine("menores de edad: " + menores);
+        }
+    }
+
+}
---
+++ while guia N3/while guia n3 tp9.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            int menor = 0;
+
+            Console.Write("ingresa un numero positivo (numero negativo para terminar): ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0)
+            {
+
+                menor = numero;
+
+                while (numero >= 0)
+                {
+
+
+                    if (numero < menor)
+                    {
+                        menor = numero;
+                    }
+
+                    Console.Write("ingresa otro número (numero negativo para terminar): ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("el numero menor es: " + menor);
+            }
+        }
+    }
+
+}
+    
