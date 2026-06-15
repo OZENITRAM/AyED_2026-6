e1312cccc28c7362c7a203050201ae6273b772dc
OZENITRAM <oliveremartinez09@gmail.com>
2026-06-15 20:34:20 -0300

    Add files via upload

---
+++ 6_Martinez_V/while guia N3/while guia N3 tp7.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre;
+
+            string estado;
+            string respuesta = "S";
+
+            int presentes = 0;
+                
+                
+            int ausentes = 0;
+            int total;
+
+            double porcentaje;
+
+            while (respuesta == "S")
+            {
+
+
+
+
+                Console.Write("ingresar nombre del alumno: ");
+
+
+                nombre = Console.ReadLine();
+
+                Console.Write("Estado (P/A): ");
+                estado = Console.ReadLine();
+
+                if (estado == "P")
+                {
+
+
+                    presentes = presentes + 1;
+
+
+                }
+                else
+                {
+                    ausentes = ausentes + 1;
+
+
+                }
+
+                Console.Write("¿desea cargar otro alumno? (S/N): ");
+                respuesta = Console.ReadLine();
+            }
+
+            total = presentes + ausentes;
+            porcentaje = (presentes * 100) / total;
+
+            Console.WriteLine("presentes: " + presentes);
+
+
+            Console.WriteLine("ausentes: " + ausentes);
+            Console.WriteLine("porcentaje de presentismo: " + porcentaje + "%");
+        }
+    }
+
+
+
+}
+    
---
+++ 6_Martinez_V/while guia N3/while guia n3 tp10.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int creditos = 100;
+            int apuesta;
+            int dado1;
+            int dado2;
+            int suma;
+
+            do
+            {
+                Console.WriteLine("creditos: " + creditos);
+
+                Console.Write("ingrese su apuesta (0 para retirarte): ");
+                apuesta = int.Parse(Console.ReadLine());
+
+                if (apuesta > 0 && apuesta <= creditos)
+                {
+                    Console.Write("ingrese el resultado del dado 1: ");
+                    dado1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("ingrese el resultado del dado 2: ");
+                    dado2 = int.Parse(Console.ReadLine());
+
+                    suma = dado1 + dado2;
+
+                    if (suma == 7 || suma == 11)
+                    {
+
+
+                        creditos = creditos + apuesta;
+
+                        Console.WriteLine("ganasteeeeeeeeeeeeee");
+                    }
+                    else
+                    {
+
+                        creditos = creditos - apuesta;
+
+                        Console.WriteLine("perdiste xd");
+
+
+
+                    }
+                }
+
+
+
+            } while (creditos > 0 && apuesta != 0);
+
+            Console.WriteLine("saldo final: " + creditos);
+        }
+    }
+
+}
