edddd3cbfe2ae6686e619da3a11640cf749e2c25
OZENITRAM <oliveremartinez09@gmail.com>
2026-08-26 17:01:07 -0300

    Add files via upload

---
+++ 6_Martinez_V/Funciones - Primeros pasos tp 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+
+
+        static void ContarRegresivo(int numero)
+        {
+            for (int i = numero; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un num para la cuenta regresiva: ");
+
+            int numero = int.Parse(Console.ReadLine());
+            ContarRegresivo(numero);
+        }
+    }
+}
